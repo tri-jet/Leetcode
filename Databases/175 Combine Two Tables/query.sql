1601b86df9375483b17f6f03ad315581fbd5829b
tri-jet <sahatrijit123@gmail.com>
2024-06-25 15:10:50 +0100

    Add different attempt comments

--- Databases/175 Combine Two Tables/query.sql
+++ Databases/175 Combine Two Tables/query.sql
@@ -11,3 +11,8 @@
 --     Else state
 -- End As state,
 -- From Person, Address;
+
+-- Second approach - didn't work because if joining, then only need to mention the left table as the right table part is mentioned in the join
+Select Person.firstName, Person.lastName, Address.city, Address.state 
+From Person, Address 
+Left Join Address On Person.personId = Address.personId
