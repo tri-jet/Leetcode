85db101660d66230ef76762878f5394cecfc30ac
tri-jet <sahatrijit123@gmail.com>
2024-09-02 12:43:53 +0100

    Update code and analyse thought process for case 2

--- 1894 Find the Student that Will Replace the Chalk/main.py
+++ 1894 Find the Student that Will Replace the Chalk/main.py
@@ -1,12 +1,9 @@
 class Solution:
     def chalkReplacer(self, chalk: List[int], k: int) -> int:
-        # must be relating to modulus of each step?
-
         students_sum = sum(chalk)
         if k >= students_sum:
-            return k % students_sum
-        else:
-            for x in range(0, len(chalk)):
-                if k-chalk[x] < 0:
-                    return x
-                k -= chalk[x]
+            k %= students_sum
+        for x in range(0, len(chalk)):
+            if k-chalk[x] < 0:
+                return x
+            k -= chalk[x]
