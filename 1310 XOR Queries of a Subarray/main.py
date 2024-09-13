0fefdf07df183a271630cc8bcc276e1580ab9131
tri-jet <sahatrijit123@gmail.com>
2024-09-13 23:13:13 +0100

    Add initial approach for prob 1310

---
+++ 1310 XOR Queries of a Subarray/main.py
@@ -0,0 +1,9 @@
+class Solution:
+    def xorQueries(self, arr: List[int], queries: List[List[int]]) -> List[int]:
+        answer = []
+        for query in queries:
+            if query[0] == query[1]:
+                answer.append(query[0])
+            else:
+                answer.append(arr[query[0]] ^ arr[query[1]])
+        return answer
