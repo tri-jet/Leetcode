38b8c9209d8a5694e9a3db9c5ec3a76326d3349c
tri-jet <sahatrijit123@gmail.com>
2024-11-30 18:12:55 +0000

    Debug index out of bounds using changing length variable

--- 443. String Compression/main.py
+++ 443. String Compression/main.py
@@ -2,18 +2,24 @@ class Solution:
     def compress(self, chars: List[str]) -> int:
         current = chars[0]
         currentCount = 1
+        charsLen = len(chars)
         for i in range(1, charsLen):
+            print(i)
+            print(chars)
             if chars[i] == current:
                 chars.pop(i)
                 currentCount += 1
+                charsLen -= 1
                 # if currentCount > 10:
             else:
                 current = chars[i]
                 chars.insert(i-1, str(currentCount))
+                charsLen += 1
                 currentCount = 1
         if currentCount > 1:
             chars.append(str(currentCount))
         return len(chars)
+                
+                
 
-
- # excludes if count > 10 + needs to add count if at end 
+        # excludes if count > 10 + needs to add count if at end 
