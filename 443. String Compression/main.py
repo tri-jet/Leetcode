d1bc673239706f290c860228127f4f6bb0b784a6
tri-jet <sahatrijit123@gmail.com>
2024-12-01 18:34:28 +0000

    Try different way using while loop

--- 443. String Compression/main.py
+++ 443. String Compression/main.py
@@ -2,24 +2,18 @@ class Solution:
     def compress(self, chars: List[str]) -> int:
         current = chars[0]
         currentCount = 1
-        charsLen = len(chars)
-        for i in range(1, charsLen):
-            print(i)
-            print(chars)
+        i = 1
+        while i <  len(chars):
+            print(len(chars))
             if chars[i] == current:
                 chars.pop(i)
                 currentCount += 1
-                charsLen -= 1
-                # if currentCount > 10:
             else:
+                chars.insert(i,str(currentCount))
                 current = chars[i]
-                chars.insert(i-1, str(currentCount))
-                charsLen += 1
                 currentCount = 1
+                i+=2
         if currentCount > 1:
             chars.append(str(currentCount))
         return len(chars)
-                
-                
-
-        # excludes if count > 10 + needs to add count if at end 
+            
