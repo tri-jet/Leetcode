a29813604db5d1ee7fe4bcfe0553defab3f29d0f
Trijit Saha <sahatrijit123@gmail.com>
2024-12-04 19:11:42 +0000

    Change array functions to refer to last and second last items
    Sort array functions sorts lowest to highest - so must do stone calculations and popping from end of array not start.

--- 1046 Last Stone Weight/main.py
+++ 1046 Last Stone Weight/main.py
@@ -2,13 +2,12 @@ class Solution:
     def lastStoneWeight(self, stones: List[int]) -> int:
         stones.sort()
         while(len(stones) > 1):
-            print(stones)
-            if(stones[0] == stones[1]):
-                stones.pop(0)
-                stones.pop(0)
+            if(stones[-1] == stones[-2]):
+                stones.pop(len(stones)-1)
+                stones.pop(len(stones)-1)
             else:
-                stones[0] -= stones[1]
-                stones.pop(1)
+                stones[-1] -= stones[-2]
+                stones.pop(-2)
                 stones.sort()
         if(len(stones) == 1):
             return stones[0]
