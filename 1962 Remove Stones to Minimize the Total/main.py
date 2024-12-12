fe0089369a5cdd28c890056cf3c78a27de2cf00a
Trijit Saha <sahatrijit123@gmail.com>
2024-12-12 20:53:36 +0000

    Subtract floored halfed value from existing pile

--- 1962 Remove Stones to Minimize the Total/main.py
+++ 1962 Remove Stones to Minimize the Total/main.py
@@ -11,13 +11,19 @@ class Solution:
         for x in range(k):
             print(piles)
             current = hq.heappop(piles)
+            print(f"current before removing: {current}")
             current *= -1
-            current = math.floor(current/2) * -1
+            current -= math.floor(float(current)/2) 
+            print(current)
+            current *= -1
             
             hq.heappush(piles, current)
-        
+            # " remove floor(piles[i] / 2) stones from it." - defining part that I missed
+        print(piles)
         piles = list(piles)
         for y in range(len(piles)):
-            piles[i] *= -1
+            piles[y] *= -1
 
         return sum(piles)
+        
+
