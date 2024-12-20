9f1d90fca0799d278278014ad04f74db4ef659d7
Trijit Saha <sahatrijit123@gmail.com>
2024-12-20 12:54:45 +0000

    Add code translated from pseudocode

---
+++ 1475 Final Prices With a Special Discount in a Shop/pseudocode_solution.py
@@ -0,0 +1,21 @@
+class Solution:
+    def finalPrices(self, prices: List[int]) -> List[int]:
+        lowest = [] # use monotonic stack for lowest prices
+        for i in range(len(prices)-1, -1, -1):
+            print(i)
+            if not lowest:
+                lowest.append(prices[i])
+                continue
+            else:
+                if lowest[-1] <= prices[i]:
+                    lowest.append(prices[i])
+                    prices[i] -= lowest[-2] 
+                else:   # if current price < top of lowest
+                    while(lowest and lowest[-1] ):   # find val lower than current or till empty
+                        lowest.pop()
+                    # now should be empty or lowest top < current
+                    lowest.append(prices[i])
+                    if len(lowest) == 2:    # if found one lower than current, use as discount
+                        prices[i] -= lowest[-2]
+                    continue
+        return prices   
