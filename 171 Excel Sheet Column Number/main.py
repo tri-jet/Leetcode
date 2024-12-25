031c86583e0742d661fbb7280f252e8a85e17fc4
Trijit Saha <sahatrijit123@gmail.com>
2024-12-25 13:32:18 +0000

    Change to account for column rows act under base 26

--- 171 Excel Sheet Column Number/main.py
+++ 171 Excel Sheet Column Number/main.py
@@ -2,6 +2,6 @@ class Solution:
     def titleToNumber(self, columnTitle: str) -> int:
         alphabet = { chr(ord('A') + i): i+1 for i in range(0,26) } # 1 line dictionary init
         colNum = 0
-        for char in columnTitle:
-            colNum += alphabet.get(char)
+        for i in range(len(columnTitle)):
+            colNum += (26^(len(columnTitle) - i-1)) * alphabet.get(columnTitle[i]) 
         return colNum
