9d0d86ec152e937422b54152bf204a5da270ac16
Trijit Saha <sahatrijit123@gmail.com>
2024-12-25 13:35:02 +0000

    Use pow function instead of ^ so that power of 0 works properly

--- 171 Excel Sheet Column Number/main.py
+++ 171 Excel Sheet Column Number/main.py
@@ -3,5 +3,5 @@ class Solution:
         alphabet = { chr(ord('A') + i): i+1 for i in range(0,26) } # 1 line dictionary init
         colNum = 0
         for i in range(len(columnTitle)):
-            colNum += (26^(len(columnTitle) - i-1)) * alphabet.get(columnTitle[i]) 
+            colNum += pow(26,(len(columnTitle) - i-1)) * alphabet.get(columnTitle[i]) 
         return colNum
