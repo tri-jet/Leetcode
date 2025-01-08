78cbad841363c9a2a9342e2d61a322f17a4c47f9
Trijit Saha <sahatrijit123@gmail.com>
2025-01-08 23:13:55 +0000

    Change to check if compared words aren't the same element
    There can be duplicate elements, but we don't want to compare a an element with the exact same element e.g. words[4] with words[4], but words[4] and words[5] is allowed

--- 3042 Count Prefix and Suffix Pairs I/main.py
+++ 3042 Count Prefix and Suffix Pairs I/main.py
@@ -1,15 +1,15 @@
 class Solution:
     
     def isPrefixAndSuffix(self, str1: str, str2: str) -> bool:
-        return (str2[0:len(str1)] == str1 and str2[len(str2)-len(str1):] == str1 and str1 != str2)
+        return (str2[0:len(str1)] == str1 and str2[len(str2)-len(str1):] == str1)
 
     def countPrefixSuffixPairs(self, words: List[str]) -> int:
         count = 0
         for i in range(len(words)):
             str1 = words[i] # don't need to store as words[i] is O(1)
             for j in range(i, len(words)):
-                if self.isPrefixAndSuffix(str1, words[j]):
+                if self.isPrefixAndSuffix(str1, words[j]) and i != j:
                     count += 1
+                    print(f"{str1} and {words[j]}")
 
         return count
-    
