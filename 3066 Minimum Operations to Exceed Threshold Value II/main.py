e8ec40ba244c22f32a77bdc9626f91151dcbc10f
Trijit Saha <sahatrijit123@gmail.com>
2025-02-14 00:27:24 +0000

    Add initial solution for p3066

---
+++ 3066 Minimum Operations to Exceed Threshold Value II/main.py
@@ -0,0 +1,14 @@
+import heapq
+class Solution:
+    def minOperations(self, nums: List[int], k: int) -> int:
+        # only need heap and then pop twice to x,y
+        # while the heap's lowest value < k, keep going
+        opCount = 0
+        heapq.heapify(nums)
+        while(nums[0] < k):
+            x = heapq.heappop(nums)
+            y = heapq.heappop(nums)
+            # can use heap push pop later
+            heapq.heappush(nums, min(x,y)*2 + max(x,y))
+            opCount += 1
+        return opCount
