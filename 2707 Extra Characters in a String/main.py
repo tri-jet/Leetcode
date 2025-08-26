e7dfaa5e01ecc4e9583ffbd9f21e68a68ec93652
tri-jet <sahatrijit123@gmail.com>
2025-08-26 23:39:02 +0100

    Remove prints from p2707

--- 2707 Extra Characters in a String/main.py
+++ 2707 Extra Characters in a String/main.py
@@ -1,8 +1,6 @@
 class Solution:
     def minExtraChar(self, s: str, dictionary: List[str]) -> int:
         for word in dictionary:
-            print(s)
             if s.find(word) != -1:
-                print("found")
                 s = s.replace(word,"",1)
         return len(s)
