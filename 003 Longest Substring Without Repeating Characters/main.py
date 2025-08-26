a5419570cecda756b089a69ec52002ad53804b8d
tri-jet <sahatrijit123@gmail.com>
2025-08-26 23:38:25 +0100

    Add initial approach for p3

---
+++ 003 Longest Substring Without Repeating Characters/main.py
@@ -0,0 +1,28 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        start = 0
+        end = 1
+        maxLen = 0
+        if len(s) == 1 or len(s) == 0:
+            return len(s)
+        found = {}
+        while(start < len(s)-1):
+            currentLen = 1
+            found = {s[start]}
+            noDupes = True
+            while(noDupes):
+                print(f"Start: {start}, end: {end}, found= {found}, currently looking at {s[end]}")
+                if s[end] not in found:
+                    found.add(s[end])
+                    currentLen += 1
+                    end += 1
+                else:
+                    print("dupe found")
+                    start += 1
+                    end = start + 1
+                    maxLen = currentLen if currentLen > maxLen else maxLen
+                    noDupes = False
+        return maxLen
+
+
+        
