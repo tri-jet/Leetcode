0e7cfc06856aade9ca9f4e1b092cec323a5ad2a6
Trijit Saha <sahatrijit123@gmail.com>
2025-12-18 16:15:03 +0000

    Add greedy full solution for p334
    Looked up solution, greedy algorithms mean not the way you think it, but being flat logical

--- 0334 Increasing Triplet Subsequence/main.py
+++ 0334 Increasing Triplet Subsequence/main.py
@@ -1,12 +1,17 @@
 class Solution:
     def increasingTriplet(self, nums: List[int]) -> bool:
-        count = 1
-        current = nums[0]
-        for x in range(1,len(nums)):
-            if x > current:
-                count += 1
-                if count == 3:
-                    return True
-                current = x
-
+        # find num smaller than infinity
+        # if x is not smaller than x1 but is less than x2 - then it means x1 < x < infinity
+        # that means if the next value is less than x1, still know we had a valid x1, x2 sequence
+        # if next value is greater than x1 but less than x2, then still got a valid x1 and new x2 sequence
+        # then if value greater than x2 and x2 - makes it a valid triplet sequence
+        x1 = float('inf')
+        x2 = float('inf')
+        for x in nums:
+            if x <= x1:
+                x1 = x
+            elif x <= x2:
+                x2 = x
+            else:
+                return True
         return False
