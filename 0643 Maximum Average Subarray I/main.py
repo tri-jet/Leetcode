ed4a2480ed656e6ce8810abef0042da47f9fc622
Trijit Saha <sahatrijit123@gmail.com>
2025-12-22 20:26:09 +0000

    Add initial attempt for p643

---
+++ 0643 Maximum Average Subarray I/main.py
@@ -0,0 +1,9 @@
+class Solution:
+    def findMaxAverage(self, nums: List[int], k: int) -> float:
+        if len(nums) == 1:
+            return float(nums[0])
+        max = 0.0
+        for x in range(0, len(nums)-k):
+            if sum(nums[x:x+k])/k > max:
+                max = sum(nums[x:x+k])/k
+        return max
