ea09eec827e3bfec93ec1e36677cea426674e5d4
Trijit Saha <sahatrijit123@gmail.com>
2025-12-22 20:33:58 +0000

    Set lowest point to negative infinity

--- 0643 Maximum Average Subarray I/main.py
+++ 0643 Maximum Average Subarray I/main.py
@@ -1,8 +1,9 @@
+import math
 class Solution:
     def findMaxAverage(self, nums: List[int], k: int) -> float:
         if len(nums) == 1:
             return float(nums[0])
-        max = 0.0
+        max = -math.inf
         for x in range(0, len(nums)-k+1):
             if sum(nums[x:x+k])/k > max:
                 max = sum(nums[x:x+k])/k
