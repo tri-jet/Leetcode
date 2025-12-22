2591faa0535a46ddd5a24a181991eb61e8e29f98
Trijit Saha <sahatrijit123@gmail.com>
2025-12-22 20:30:45 +0000

    Fix loop range as was excluding last number

--- 0643 Maximum Average Subarray I/main.py
+++ 0643 Maximum Average Subarray I/main.py
@@ -3,7 +3,7 @@ class Solution:
         if len(nums) == 1:
             return float(nums[0])
         max = 0.0
-        for x in range(0, len(nums)-k):
+        for x in range(0, len(nums)-k+1):
             if sum(nums[x:x+k])/k > max:
                 max = sum(nums[x:x+k])/k
         return max
