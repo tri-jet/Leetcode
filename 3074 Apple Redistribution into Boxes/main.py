cec7f9ef8a138be1df02feff809d4b00d62d58db
Trijit Saha <sahatrijit123@gmail.com>
2025-12-24 14:52:03 +0000

    Fix p3074 for all test cases

--- 3074 Apple Redistribution into Boxes/main.py
+++ 3074 Apple Redistribution into Boxes/main.py
@@ -6,7 +6,7 @@ class Solution:
        for x in range(len(capacity)-1, -1, -1):
         if totalApples == 0:
             return boxCount
-        if capacity[x] <= totalApples:
+        if totalApples > 0 :
             boxCount += 1
             totalApples -= capacity[x]
        return boxCount 
