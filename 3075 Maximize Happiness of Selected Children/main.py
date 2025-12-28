555b4be1202eeedd4f6b0025e9e3ee9bad20d87c
Trijit Saha <sahatrijit123@gmail.com>
2025-12-28 16:58:08 +0000

    Speed up each step of process for runtime by not popping from happiness list
    Updated the logic for calculating maximum happiness by selecting values from the sorted list in descending order instead of popping the used value, and choosing the new last value

--- 3075 Maximize Happiness of Selected Children/main.py
+++ 3075 Maximize Happiness of Selected Children/main.py
@@ -1,13 +1,12 @@
 class Solution:
     def maximumHappinessSum(self, happiness: List[int], k: int) -> int:
         # sort them 
-        # take the top value - remove from list, add to sum, increment pickCount i.e. first time 0 ->1
+        # select from end in descending order, add to sum, increment pickCount i.e. first time 0 ->1
         # repeat till pickCount = k, and also when picking child, subtract pickCount from their happiness
         happiness.sort()
         picked = 0
         happySum = 0
         while picked < k:
-            happySum += happiness[-1]-picked if happiness[-1]-picked > 0 else 0
-            happiness.pop()
+            happySum += happiness[-1-picked]-picked if happiness[-1-picked]-picked > 0 else 0
             picked += 1
         return happySum
