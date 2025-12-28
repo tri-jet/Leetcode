310512f4c607c710cd7344ccca9ef21bbc28c6b2
Trijit Saha <sahatrijit123@gmail.com>
2025-12-28 16:49:55 +0000

    Add initial attempt at 3075

---
+++ 3075 Maximize Happiness of Selected Children/main.py
@@ -0,0 +1,13 @@
+class Solution:
+    def maximumHappinessSum(self, happiness: List[int], k: int) -> int:
+        # sort them 
+        # take the top value - remove from list, add to sum, increment pickCount i.e. first time 0 ->1
+        # repeat till pickCount = k, and also when picking child, subtract pickCount from their happiness
+        happiness.sort()
+        picked = 0
+        happySum = 0
+        while picked < k:
+            happySum += happiness[-1]-picked
+            happiness.pop()
+            picked += 1
+        return happySum
