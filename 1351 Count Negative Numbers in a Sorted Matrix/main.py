b1441bdcee174c5854c774492a6d3d734f47d51c
Trijit Saha <sahatrijit123@gmail.com>
2025-12-31 20:30:01 +0000

    Create rowLen variable and set end to that instead of len(grid)

--- 1351 Count Negative Numbers in a Sorted Matrix/main.py
+++ 1351 Count Negative Numbers in a Sorted Matrix/main.py
@@ -1,10 +1,11 @@
 class Solution:
     def countNegatives(self, grid: List[List[int]]) -> int:
         count = 0
+        rowLen = len(grid[0])
         for row in grid:
             start = 0
-            end = len(grid)-1
-            first_neg = len(grid[0])
+            end = rowLen - 1
+            first_neg = rowLen
 
             while start <= end:
                 mid = (start + end) //2
@@ -12,5 +13,5 @@ class Solution:
                     first_neg = mid
                     end = mid - 1
                 else: start = mid + 1
-            count += len(grid[0]) - first_neg
+            count += rowLen - first_neg
         return count
