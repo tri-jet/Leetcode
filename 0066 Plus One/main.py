6a84586f8093b65b6e3e550f15e6ad6dc24c2341
Trijit Saha <sahatrijit123@gmail.com>
2026-01-02 15:56:38 +0000

    Add initial solution for p66

---
+++ 0066 Plus One/main.py
@@ -0,0 +1,10 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        carry = 1
+        for x in range(len(digits)-1, -1, -1):
+            currentSum = digits[x] + carry
+            digits[x] = currentSum % 10
+            carry = currentSum // 10
+        if carry == 1:
+            digits = [1] + digits
+        return digits
