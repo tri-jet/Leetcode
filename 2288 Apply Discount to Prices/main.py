997f7cd1105a4758ec664829dc2d5ec4b4245d16
Trijit Saha <sahatrijit123@gmail.com>
2026-01-02 17:07:01 +0000

    Remove 0 discount check as still need to reformat with 2dp

--- 2288 Apply Discount to Prices/main.py
+++ 2288 Apply Discount to Prices/main.py
@@ -3,8 +3,6 @@ class Solution:
     def discountPrices(self, sentence: str, discount: int) -> str:
         # all words split by spaces
         # doesn't say if initial prices can include decimals - assuming not then
-        if not discount:
-            return sentence
         words = sentence.split()
         for x in range(0, len(words)):
             # ^  = start, \$ = dollar sign, \d+ = one or more digits, $  = end of the word
