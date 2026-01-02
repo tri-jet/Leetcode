129c434be0b8f5934dbd8014afa82effcffc134b
Trijit Saha <sahatrijit123@gmail.com>
2026-01-02 17:02:36 +0000

    Add initial attempt at 2288
    After some tweaking of the Regex pattern

---
+++ 2288 Apply Discount to Prices/main.py
@@ -0,0 +1,14 @@
+import re
+class Solution:
+    def discountPrices(self, sentence: str, discount: int) -> str:
+        # all words split by spaces
+        # doesn't say if initial prices can include decimals - assuming not then
+        words = sentence.split()
+        for x in range(0, len(words)):
+            # ^  = start, \$ = dollar sign, \d+ = one or more digits, $  = end of the word
+            priceFound = re.search(r"\$\d+$",words[x])
+            if priceFound:
+                price = float(priceFound.string[1:])
+                newPrice = price - price*discount/100
+                words[x] = f"${newPrice:.2f}"
+        return " ".join(words)
