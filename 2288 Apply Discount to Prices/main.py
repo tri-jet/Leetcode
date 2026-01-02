45de93b5977ccc797493564e47031d6e69b59112
Trijit Saha <sahatrijit123@gmail.com>
2026-01-02 17:05:53 +0000

    Update p2288 for 0 discount and reinstate start part of regex

--- 2288 Apply Discount to Prices/main.py
+++ 2288 Apply Discount to Prices/main.py
@@ -3,10 +3,12 @@ class Solution:
     def discountPrices(self, sentence: str, discount: int) -> str:
         # all words split by spaces
         # doesn't say if initial prices can include decimals - assuming not then
+        if not discount:
+            return sentence
         words = sentence.split()
         for x in range(0, len(words)):
             # ^  = start, \$ = dollar sign, \d+ = one or more digits, $  = end of the word
-            priceFound = re.search(r"\$\d+$",words[x])
+            priceFound = re.search(r"^\$\d+$",words[x])
             if priceFound:
                 price = float(priceFound.string[1:])
                 newPrice = price - price*discount/100
