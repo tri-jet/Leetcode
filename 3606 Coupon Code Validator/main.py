b8814780091e5f1c2e38bd3a506089a1389eb50f
Trijit Saha <sahatrijit123@gmail.com>
2026-01-03 22:59:52 +0000

    Replace is identifier with regex for word chars for valid code + check for empty code

--- 3606 Coupon Code Validator/main.py
+++ 3606 Coupon Code Validator/main.py
@@ -1,14 +1,19 @@
+import re
 class Solution:
     def validateCoupons(self, code: List[str], businessLine: List[str], isActive: List[bool]) -> List[str]:
-        # coupon validity = isidentifier - unless includes start w/ number - will try with this first
+        # coupon validity - needs number at start - \w works actually from re instead of using all the types
         # put in separate lists, then .sort each at end, then append in business line order.
+        # use full match as search means it looks for just anything matching - so search \w+ would look for any matches for word characters
+        # And w means look for 1 word character, \w+ means 1 or more
         validElec = []
         validGroc = []
         validPharm = []
         validRest = []
 
         for x in range(len(code)):
-            if code[x].isidentifier():
+            if code[x] == "":
+                continue
+            if re.fullmatch(r"\w+", code[x]):
                 if isActive[x]:
                     match businessLine[x]:
                         case "electronics":
