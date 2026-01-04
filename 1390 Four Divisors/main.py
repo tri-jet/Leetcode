e740f6fe7c1a8195d6492a287517082d86fc5079
Trijit Saha <sahatrijit123@gmail.com>
2026-01-04 18:59:31 +0000

    Find factors up to square root of number + 1 to speed up divisors

--- 1390 Four Divisors/main.py
+++ 1390 Four Divisors/main.py
@@ -1,14 +1,14 @@
+import math
 class Solution:
     def sumFourDivisors(self, nums: List[int]) -> int:
         finalSum = 0
         for x in nums:
             divisors = []
-            for i in range(2, int(x/2) + 1):
+            for i in range(1, int(math.sqrt(x) + 1)):
                 if x % i == 0:
                     divisors.append(i)
-            divisors.append(1)
-            if x != 1:
-                divisors.append(x)
+                    if i != x/i:
+                        divisors.append(int(x/i))
             if len(divisors) == 4:
                 finalSum += sum(divisors)
-        return finalSum
+        return int(finalSum)
