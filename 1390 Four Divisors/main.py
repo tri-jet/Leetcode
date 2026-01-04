2a14314007b3093f066c3f839c7aceb075f1799f
Trijit Saha <sahatrijit123@gmail.com>
2026-01-04 01:00:08 +0000

    Fix divisor range calculation to simpler n/2 + 1
    Just do a simple range rather than some OTT ceiling divisor.
    Still only 13/18 testcases passed - failed due to taking too long.

--- 1390 Four Divisors/main.py
+++ 1390 Four Divisors/main.py
@@ -1,10 +1,9 @@
 class Solution:
     def sumFourDivisors(self, nums: List[int]) -> int:
-        #-(a // -b) - a/b but round up as ceil "can quietly produce incorrect results, as it introduces floating-point error"
         finalSum = 0
         for x in nums:
             divisors = []
-            for i in range(2, -(x // -2)):
+            for i in range(2, int(x/2) + 1):
                 if x % i == 0:
                     divisors.append(i)
             divisors.append(1)
