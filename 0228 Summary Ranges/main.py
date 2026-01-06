dd9c4d0ebb49e9c78d9bafc4016f8c03950b0c76
Trijit Saha <sahatrijit123@gmail.com>
2026-01-06 00:19:12 +0000

    Add initial attempt (messy) at p228

---
+++ 0228 Summary Ranges/main.py
@@ -0,0 +1,24 @@
+import math
+class Solution:
+    def summaryRanges(self, nums: List[int]) -> List[str]:
+        # basically ranges = consecutive lists
+        ranges = []
+        start = current = -math.inf
+        for x in range(0, len(nums)):
+            print(f"ranges: {ranges}, x: {x}, num = {nums[x]}")
+            print(f"start: {start}, current: {current}")
+            if start == -math.inf and current == -math.inf:
+                start = nums[x]
+                current = nums[x]
+            elif nums[x] == current + 1:
+                current = nums[x]
+            else:
+                if current == start:
+                    ranges.append(str(int(start))) 
+                else: ranges.append(str(int(start)) + "->" + str(int(current)))
+                start = nums[x]
+                current = nums[x]
+        if current == start:
+            ranges.append(str(int(start)))
+        else: ranges.append(str(int(start)) + "->" + str(int(current)))
+        return ranges
