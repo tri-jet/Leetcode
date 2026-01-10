770419d1dd824449bc4f6a94979f9bd230cd8f2f
Trijit Saha <sahatrijit123@gmail.com>
2026-01-10 00:14:30 +0000

    Fix comparing lists of frequencies of chars in two words
    Turns out lists need to be sorted in same order to be equal - so lots of sorts in this solution it feels like

--- 1657 Determine if Two Strings Are Close/main.py
+++ 1657 Determine if Two Strings Are Close/main.py
@@ -7,8 +7,8 @@ class Solution:
         l1.sort()
         l2.sort()
         print(f"L1: {l1}, \n L2: {l2}")
-        # if l1 == l2:
-        #     return True
+        if l1 == l2:
+            return True
         # use unique num of occurrences if occurences match then works?
         # count frequencies of different chars - but doesn't actually matter what they are?
         # e.g. case 3 - cabbba vs abbccc = 1 x c, 2 x a, 3 x b vs 1 x a, 2 x b, 3 x c
@@ -21,9 +21,14 @@ class Solution:
             l2Hash[l2[x]] = l2Hash.get(l2[x],0) + 1
             print(f"l1 current: {l1[x]}")
             print(f"l2 current: {l2[x]}")
-        print(l1Hash.keys())
-        print(l2Hash.keys())    # printed empty?? for both
-        return l2Hash.keys() == l1Hash.keys()
+        print(f"{l1Hash.keys()} and values: {l1Hash.values()}")
+        print(f"{l2Hash.keys()} and values: {l2Hash.values()}")   # printed empty?? for both
+        l1 = list(l2Hash.values())
+        l1.sort()
+        l2 = list(l1Hash.values())
+        l2.sort()
+        print(f"now l1: {l1} now l2: {l2}")
+        return l1 == l2
 
         # failed for cabbba and aabbss - output was true
         # cabbba = 1c, 2a, 3b vs aabbss = 2a 2b 2s 
