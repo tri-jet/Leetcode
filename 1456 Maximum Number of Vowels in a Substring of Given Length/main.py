0a6195989f709900ec33b7dd91545bced34710b2
Trijit Saha <sahatrijit123@gmail.com>
2026-01-14 11:53:08 +0000

    Fix loop range for p1456
    Updated the loop range to handle the last substring correctly so it worked for "weallloveyou"
    
    but note: needs to be optimised as passed 100/107 testcases, but didn't work for a really long string - obvs since O(n x m)

--- 1456 Maximum Number of Vowels in a Substring of Given Length/main.py
+++ 1456 Maximum Number of Vowels in a Substring of Given Length/main.py
@@ -3,18 +3,21 @@ class Solution:
         # at least O(n) - need to check every pos to see max count of vowels in window
         # look at every k letters and count num of vowels - can only range from 0 to k. So if found k then return no point finding more
         count = 0
-        for x in range(k, len(s)):
-            
-            print(s[x-k:x])
+        for x in range(k, len(s)+1):
+            #print(f"{s[x-k:x]} - which is x-k: {x-k} to x: {x}")
             current = 0
             for letter in s[x-k:x]:
-                
                 if letter in ['a','e','i','o','u']:
                     current += 1
-                
-                print(f"letter: {letter}, current: {current}")
-            print(f"ending search of {s[x-k:x]}, current was {current}, count previously {count}")
+                if current == k:
+                    return k
+                #print(f"letter: {letter}, current: {current}")
+            #print(f"ending search of {s[x-k:x]}, current was {current}, count previously {count}")
             count = current if current > count else count
-            print(f"so count now: {count}")
+            #print(f"so count now: {count}")
 
         return count
+
+# so for some reason unable to do last 7 chars of weallloveyou k = 7 i.e. loveyou - i.e. 4
+# len of "weallloveyou" is 12
+# x in range k to len -> so that's 7 to 12. but that's actually 
