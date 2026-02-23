8f512a9bfe867286c1629361e3f954963a904eda
Trijit Saha <sahatrijit123@gmail.com>
2026-02-23 14:05:38 +0000

    Remove current gap variable for p868
    Runtime 0ms
    Beats 100.00%
    Memory 19.46MB
    Beats 32.09%

--- 0868 Binary Gap/main.py
+++ 0868 Binary Gap/main.py
@@ -3,12 +3,9 @@ class Solution:
         maxGap = 0
         binStr = bin(n)[2:]
         startIndex = 0   # start will always be 1 as no leading zeroes included
-        currentGap = 0  # do I need this if I have start and end index? probs not
         for x in range(1, len(binStr)):
             if binStr[x] == "1":
-                currentGap = x-startIndex
-                if currentGap > maxGap:
-                    maxGap = currentGap
+                if x-startIndex > maxGap:
+                    maxGap = x-startIndex
                 startIndex = x
-                currentGap = 0
         return maxGap
