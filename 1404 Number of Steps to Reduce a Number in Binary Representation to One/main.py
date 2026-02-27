3040504e6dce81dfb662179a9855087888d4e06c
Trijit Saha <sahatrijit123@gmail.com>
2026-02-27 09:33:24 +0000

    Change division to floor division, as that keeps as an int.
    / -> converts number to float - loses some some precision - even if result is whole number.
    // -> floor division, keeps it as an int, maintain precision - and allow right number of steps.

--- 1404 Number of Steps to Reduce a Number in Binary Representation to One/main.py
+++ 1404 Number of Steps to Reduce a Number in Binary Representation to One/main.py
@@ -4,7 +4,7 @@ class Solution:
         steps = 0
         while number > 1:
             if number % 2 == 0:
-                number /= 2
+                number //= 2
             else: number += 1
             steps += 1
         return steps
