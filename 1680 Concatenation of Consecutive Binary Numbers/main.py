196c2916ecc3d010945bf67b4f3cbfbe3bb3c54c
Trijit Saha <sahatrijit123@gmail.com>
2026-02-28 18:34:08 +0000

    Add initial attempt at p1680
    Passed 2/3 of first testcases

---
+++ 1680 Concatenation of Consecutive Binary Numbers/main.py
@@ -0,0 +1,6 @@
+class Solution:
+    def concatenatedBinary(self, n: int) -> int:
+        concatStr = ""
+        for x in range(1, n+1):
+            concatStr += bin(x)[2:]
+        return int(concatStr,2)
