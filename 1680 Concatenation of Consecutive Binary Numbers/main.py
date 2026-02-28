985c26f4c8b20176461f0ef1924ecab36017ea90
Trijit Saha <sahatrijit123@gmail.com>
2026-02-28 18:38:42 +0000

    Add the modulo 10^9 + 7 to the result

--- 1680 Concatenation of Consecutive Binary Numbers/main.py
+++ 1680 Concatenation of Consecutive Binary Numbers/main.py
@@ -3,4 +3,4 @@ class Solution:
         concatStr = ""
         for x in range(1, n+1):
             concatStr += bin(x)[2:]
-        return int(concatStr,2)
+        return int(concatStr,2) % 1000000007
