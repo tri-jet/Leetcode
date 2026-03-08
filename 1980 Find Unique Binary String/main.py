1f60e5202469ef7208dc44aab0e7f08c72723b37
Trijit Saha <sahatrijit123@gmail.com>
2026-03-08 19:24:24 +0000

    Add initial attempt at p1980

---
+++ 1980 Find Unique Binary String/main.py
@@ -0,0 +1,15 @@
+class Solution:
+    def findDifferentBinaryString(self, nums: List[str]) -> str:
+        # generate all combos first? then strike off?
+        # hint: convert given strings to base 10
+        # hmm ok, so n = 2, -> range 00 -> 11 = 00 to 3
+        # can loop thru 0 to 2^n -1, find first num that exists in remaining set?
+
+        n = len(nums[0])
+        print(n)
+        limit = 2^n # n=3 range = 0 to 7, range(a, b) goes from a to b-1 anyways 
+        nums = set(nums)    # check if in = O(1)
+        for x in range(0, limit):
+            print(f"x:{x}, bin={bin(x)[2:]}")
+            if bin(x)[2:] not in nums:
+                return bin(x)[2:]
