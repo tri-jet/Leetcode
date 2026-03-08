794678812a0b85eb89ebe9a15fd94b2c7d35ac5c
Trijit Saha <sahatrijit123@gmail.com>
2026-03-08 19:30:36 +0000

    Add correct exponent for Python and add 0 binary string handling

--- 1980 Find Unique Binary String/main.py
+++ 1980 Find Unique Binary String/main.py
@@ -7,9 +7,15 @@ class Solution:
 
         n = len(nums[0])
         print(n)
-        limit = 2^n # n=3 range = 0 to 7, range(a, b) goes from a to b-1 anyways 
+        print(2**n)
+        limit = 2**n # n=3 range = 0 to 7, range(a, b) goes from a to b-1 anyways 
         nums = set(nums)    # check if in = O(1)
         for x in range(0, limit):
             print(f"x:{x}, bin={bin(x)[2:]}")
-            if bin(x)[2:] not in nums:
+            if x == 0:
+                if "0"*n not in nums:
+                    return "0"*n
+            elif bin(x)[2:] not in nums:
                 return bin(x)[2:]
+
+        # 0 won't show as 0b00, or whatever length.
