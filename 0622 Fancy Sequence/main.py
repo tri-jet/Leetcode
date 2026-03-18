aa7eec3aa57b82e4fe6251e22b65f0162543e625
Trijit Saha <sahatrijit123@gmail.com>
2026-03-18 11:56:09 +0000

    Add changes to a list so only do change on the index retrieved
    Added a changes list to track operations performed on fancySeq.

--- 0622 Fancy Sequence/main.py
+++ 0622 Fancy Sequence/main.py
@@ -1,24 +1,32 @@
 class Fancy:
     fancySeq = []
+    changes = []
     def __init__(self):
-        self.fancySeq = []
+        self.fancySeq.clear()
+        self.changes.clear()
 
     def append(self, val: int) -> None:
         self.fancySeq.append(val)
 
     def addAll(self, inc: int) -> None:
-        for x in range(0,len(self.fancySeq)):
-            self.fancySeq[x] += inc
+        self.changes.append(f"+{inc}")
 
     def multAll(self, m: int) -> None:
-        for x in range(0,len(self.fancySeq)):
-            self.fancySeq[x] *= m
+        self.changes.append(f"*{m}")
         
 
     def getIndex(self, idx: int) -> int:
+        print(self.fancySeq)
+        print(self.changes)
         if idx >= len(self.fancySeq):
             return -1
-        return self.fancySeq[idx] % (10**9 + 7)
+        value = self.fancySeq[idx]
+        for op in self.changes:
+            if op[0] == "+":
+                value += int(op[1:])
+            elif op[0] == "*":
+                value *= int(op[1:])
+        return value % (10**9 + 7)
 
 
 # Your Fancy object will be instantiated and called as such:
