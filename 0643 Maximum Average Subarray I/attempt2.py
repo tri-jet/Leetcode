939c05c83a756f7d069c01c96cf68ac7dc8cd110
Trijit Saha <sahatrijit123@gmail.com>
2026-04-09 23:07:15 +0100

    Fix sliding window index - now fully passed
    Runtime 
    250 ms Beats 5.09%
    Memory
    29.61 MB Beats 11.57%

--- 0643 Maximum Average Subarray I/attempt2.py
+++ 0643 Maximum Average Subarray I/attempt2.py
@@ -2,11 +2,12 @@ class Solution:
     def findMaxAverage(self, nums: List[int], k: int) -> float:
         startSum = sum(nums[0:k])
         avg = startSum / k
-        for x in range(1,len(nums)-k):
+        for x in range(0,len(nums)-k):
+            print(f"x: {x}")
             # sliding window, so drop first, and add new end
             #startSum = nums[0+x:x+k]
-            startSum -= nums[x-1] # remove start
-            startSum += nums[k+x-1]
+            startSum -= nums[x] # remove start
+            startSum += nums[k+x]
             if startSum/k > avg:
                 avg = startSum/k
         return avg
