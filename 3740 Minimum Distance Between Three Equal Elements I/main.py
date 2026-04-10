762c0252d4243625065e64186bcd423b8924a355
Trijit Saha <sahatrijit123@gmail.com>
2026-04-10 12:31:22 +0100

    Fix p3740 - closest distance doesn't mean leftmost as well
    Refactor distance calculation for three equal elements and manage old indices.

--- 3740 Minimum Distance Between Three Equal Elements I/main.py
+++ 3740 Minimum Distance Between Three Equal Elements I/main.py
@@ -11,13 +11,14 @@ class Solution:
             else:
                 if len(goodTuples.get(nums[x])) < 3:
                     goodTuples[nums[x]].append(x)
-                    print(f"Adding at: {x}, and nums[x] is now: {goodTuples[nums[x]]}")
                 if len(goodTuples.get(nums[x])) == 3:
-                    dist = goodTuples.get(nums[x])[1]-goodTuples.get(nums[x])[0] + x -goodTuples.get(nums[x])[1] + x - goodTuples.get(nums[x])[0]
-                    print(f"dist w/ val being {nums[x]} is: {dist}")
+                    dist = goodTuples.get(nums[x])[1]-goodTuples.get(nums[x])[0] 
+                    dist += goodTuples.get(nums[x])[2]-goodTuples.get(nums[x])[1] 
+                    dist += goodTuples.get(nums[x])[2] - goodTuples.get(nums[x])[0]
                     if minDist == -1:
                         minDist = dist
                     elif dist < minDist:
                         minDist = dist
-        print(goodTuples)
+                    # remove old first 
+                    goodTuples[nums[x]].pop(0)
         return minDist
