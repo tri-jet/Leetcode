9f83e4cc77247ab74113c911a126ad388edc5876
Trijit Saha <sahatrijit123@gmail.com>
2026-04-13 00:06:04 +0100

    Add proper bfs solution for p199 - looked at solution

--- 199 Binary Tree Right Side View/main.py
+++ 199 Binary Tree Right Side View/main.py
@@ -6,14 +6,22 @@
 #         self.right = right
 class Solution:
     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
-        # only right - so always exclude the left side
-        # couldn't this be dfs too?
-        rights = []
-        def dfs(node, rightList):
-            if not node:
-                return
-            rightList.append(node.val)
-            dfs(node.right, rightList)
         
-        dfs(root,rights)
+        # need to check levels + prefer right, but use left if no right, so bfs
+        # or use dfs - but check which levels need left usage
+        rights = []
+        q = []  # to look at
+        q.append(root)
+
+        while q:
+            right = None
+            for x in range(len(q)): # only goes for q at the time of running the for loop first time.
+                node = q.pop(0)
+                if node:
+                    right = node
+                    q.append(node.left)
+                    q.append(node.right)
+            if right:
+                rights.append(right.val)
+
         return rights
