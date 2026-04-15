f3190eff2193ad56a22da840823013f138b1b691
Trijit Saha <sahatrijit123@gmail.com>
2026-04-15 16:37:29 +0100

    Add solution to p2515

--- 2515 Shortest Distance to Target String in a Circular Array/main.py
+++ 2515 Shortest Distance to Target String in a Circular Array/main.py
@@ -2,12 +2,17 @@ class Solution:
     def closestTarget(self, words: List[str], target: str, startIndex: int) -> int:
         # search for index of target, then just do the calc
         if words[startIndex] == target:
-            return startIndex
-        if words.count(target) == 0:
+            return 0
+        targetCount = words.count(target)
+        if targetCount == 0:
             return -1
-        index = words.index(target)
+        
         # but assumes first occurrence is closest - but assuming no dupplicates?
-
-        if index < startIndex:
-            return startIndex-index
-        else: return (index-startIndex)
+        minDist = 101
+        while(targetCount > 0):
+            index = words.index(target)
+            if min(abs(startIndex-index), len(words)-abs(startIndex-index)) < minDist:
+                minDist = min(abs(startIndex-index), len(words)-abs(startIndex-index))
+            words[index] = "*"
+            targetCount -= 1
+        return minDist
